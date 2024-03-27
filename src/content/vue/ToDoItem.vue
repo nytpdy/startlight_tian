731b3901b188a94d6588507e31f24395fc76cb74
nie <tiande.nie@lucis-tech.com>
2024-03-27 16:36:37 +0800

    添加一个vue的测试界面

---
+++ src/content/vue/ToDoItem.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <div id ="app">
+    <input type="checkbox" id="todo-item" checked="false" />
+    <label for="todo-item">{{ label }}</label>
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  data() {
+    return {
+      label: 'hello world',
+    }
+  },
+}
+</script>
